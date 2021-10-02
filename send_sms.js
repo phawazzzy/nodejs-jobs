6cdd09a16566c19d387a3a3d51af50288a9243cc
fawas kareem <phawazzzy@gmail.com>
2021-10-02 14:32:58 +0100

    added content

--- send_sms.js
+++ send_sms.js
@@ -2,7 +2,8 @@ const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-const message = `Mr yomi. This is Nurudeen, I'm sure you remember me right.
+const message = `GBESE REMINDER \n
+Mr yomi. This is Nurudeen, I'm sure you remember me right.
 If you don't remember me, I'm the guy that you owe 56k.
 Please pay my money to this account. \n 
 0241259669, wema \n
@@ -18,7 +19,7 @@ module.exports = function sms() {
             .create({
                 body: message,
                 from: '+17738230851',
-                to: '+2348184303931'
+                to: '+2348162816543'
             })
             .then(message => console.log(message.sid));
     } catch (error) {
